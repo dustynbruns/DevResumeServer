7db5d37cbf7c50ef392a4d6fe21d2e664349ca87
Dustyn Brunse <dustyn79087@gmail.com>
2023-06-08 15:21:25 -0500

    updated PORT

--- server.js
+++ server.js
@@ -69,6 +69,8 @@ app.get('/projects', async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
